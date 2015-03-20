89303b5949cf9cc1060074302ae953df34b982b0
sutantyo <daniel.sutantyo@gmail.com>
2015-03-20 12:26:08 +1100

    can draw from csv

--- fileConversion/convert_to_csv.js
+++ fileConversion/convert_to_csv.js
@@ -1,91 +1,22 @@
-var pg = require('pg');
+// Code stolen from https://strongloop.com/strongblog/practical-examples-of-the-new-node-js-streams-api/
+
 var fs = require('fs');
-var async = require ('async');
+var liner = require('./liner');
+var source = fs.createReadStream('./taxi_february.txt');
+var destination = fs.createWriteStream('./taxi_february.csv');
 
-var connectionString = "postgres://daniels:macquarie@localhost/crawdad";
-//var client = new pg.Client(conString);
-
-pg.defaults.poolsize = 25;
-
-pg.connect(connectionString, function(err,client,done){
-  if(err) {
-    return console.error('could not connect to postgres', err);
-  }
-	
-	async.series([
-		function(callback){
-			client.query("DROP TABLE IF EXISTS taxi_roma", function(err,result){
-				if (err)
-					return console.error('could not drop table', err);
-				done();
-				callback(null,'one');
-			});
-		},
-		function(callback){
-			client.query("CREATE TABLE IF NOT EXISTS taxi_roma (id smallint, date date, time time, x decimal, y	decimal)", function(err,result){
-				if (err)
-					return console.error('could not create table', err);
-				else
-					done();
-					callback(null,'two');
-			});
-		},
-		function(callback){
-			fs.readFile('taxi_february_1','utf-8',function(err,data){
-				if(err) 
-					return console.error('error opening file', err);
+source.pipe(liner);
+liner.on('readable',function(){
+	var line;
+	while (line = liner.read()) {
+		parts = line.split(';');
+		var id = parts[0];
+		var datetime = parts[1].substring(0,19).split(' ');
+		var startBracket = parts[2].indexOf('(')+1;
+		var endBracket 	 = parts[2].lastIndexOf(')');
+		var xy = parts[2].substring(startBracket,endBracket).split(' ');
 		
-				var lines = data.split('\n');
-				async.eachSeries(lines, function(line,callback){
-					if (line){
-						parts = line.split(';');
-						var id = parts[0];
-						var datetime = parts[1].substring(0,19).split(' ');
-						var startBracket = parts[2].indexOf('(')+1;
-						var endBracket 	 = parts[2].lastIndexOf(')');
-						var xy = parts[2].substring(startBracket,endBracket).split(' ');
-
-								/*
-								console.log("INSERT INTO taxi_roma VALUES (" + id + ", '" 
-																															+ datetime[0] + "', '" 
-																															+ datetime[1] + "', "
-																															+ xy[0] + ", " 
-																															+ xy[1] + ");");
-								*/
-						client.query("INSERT INTO taxi_roma VALUES (" + id + ", '" 
-																															+ datetime[0] + "', '" 
-																															+ datetime[1] + "', "
-																															+ xy[0] + ", " 
-																															+ xy[1] + ");", function(err,result){
-								if(err)	{
-									return console.error('Error inserting query', err);
-								}	
-								done();
-								callback();
-							});
-						}
-			  else
-				{
-					console.log(line + "Done");
-					callback(null,'three');
-				}
-			});
-		});
-	},
-	function(callback){
-		console.log("Closed connection");
-		client.end();
-		callback(null,'four');
+		//console.log(id + ';' + datetime[0] + ';' + datetime[1] + ';' + xy[0] + ';' + xy[1]);
+		destination.write(id + ',' + datetime[0] + ',' + datetime[1] + ',' + xy[0] + ',' + xy[1] + '\n');
 	}
-	],
-	function(err,results){
-		if(err){
-			return console.error('error in series', err);
-		}
-		console.log("Series finished: " + results);
-	});
-});	
-
-
-
-
+});
